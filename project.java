a0a73f3d1aabb6eb5e4967c3cba8cb806a5ad7c4
nguyenquocnhat <nguyenquocnhat25022002@gmail.com>
2022-03-18 20:02:57 +0700

    Chinh sua ham main va them chuc nang chon phep tinh

--- project.java
+++ project.java
@@ -1,25 +1,47 @@
 import java.util.Scanner;
 
 public class project {
-    static void tinhtong(int a, int b){
+    static int tinhtong(int a, int b){
 
     }
-    static void tinhhieu(int a,int b){
+    static int tinhhieu(int a,int b){
 
     }
-    static void tinhtich(int a,int b){
+    static int tinhtich(int a,int b){
 
     }
-    static void tinhthuong(int a, int b){
+    static float tinhthuong(int a, int b){
 
     }
 
     public static void main(String[] args) {
-        Scanner sv = new Scanner(System.in);
-        System.out.println("Nhap a:");
-        int a = sv.nextInt();
-        System.out.println("Nhap b:");
-        int b = sv.nextInt();
-        //Them chuc nang chon phep tinh cho nay
+        int a,b;
+		char chon,y;
+		do {
+			Scanner objSc = new Scanner(System.in);
+			System.out.println("Nhap so a :");
+			a = objSc.nextInt();
+			System.out.println("Nhap so b :");
+			b = objSc.nextInt();
+			System.out.println("Hay nhap phep tinh (+,-,*,/)");
+			chon = (char) System.in.read();
+			switch(chon) {
+			case '+':System.out.println("Tong hai so la :"+tinhtong(a,b));
+					break;
+			case '-':System .out.println("Hieu hai so la :"+tinhhieu(a,b));
+					break;
+			case '*':System.out.println("Tich hai so la "+tinhtich(a,b));
+					break;
+			case '/':System.out.println("Thuong hai so la "+tinhthuong(a,b));
+					break;
+			default :System.out.println("Khong dung hay nhap lai nhe :");
+					break;
+			}
+			System.in.skip(chon);
+			System.out.println("Tiep tuc khong yes or no(y/n):");
+			y = (char)System.in.read();
+			
+			
+		}while(y=='Y'||y=='y');
     }
 }
